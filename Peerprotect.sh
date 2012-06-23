9025163f1f00dae258948c89b57a372213e42755
Joseph Iacoviello <joe.v.iacoviello@gmail.com>
2012-06-23 19:53:30 -0400

    added README and made some changes to Peerprotect

--- Peerprotect.sh
+++ Peerprotect.sh
@@ -10,6 +10,7 @@ portstop=6999
 update="update"
 start="start"
 stop="stop"
+reload="reload"
 
 
 if [ "$1" = "$update" ]; then
@@ -64,10 +65,38 @@ elif [ "$1" = "$stop" ]; then
 	iptables -X $filtername &> /dev/null
 	exit
 
+elif [ "$1" = "$reload" ]; then
+	echo "Flushing"
+	iptables -F $filtername
+	iptables -D INPUT -p tcp --dport $portstart:$portstop -j $filtername
+	iptables -D INPUT -p udp --dport $portstart:$portstop -j $filtername
+	iptables -D OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT &> /dev/null
+	iptables -D OUTPUT -p tcp -j $filtername &> /dev/null
+	iptables -D OUTPUT -p udp -j $filtername &> /dev/null
+	iptables -X $filtername &> /dev/null
+
+	echo "Adding rules"
+	iptables -F $filtername &> /dev/null
+	iptables -D INPUT -p tcp --dport $portstart:$portstop -j $filtername &> /dev/null
+	iptables -D INPUT -p udp --dport $portstart:$portstop -j $filtername &> /dev/null
+	iptables -D OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT &> /dev/null
+	iptables -D OUTPUT -p tcp -j $filtername &> /dev/null
+	iptables -D OUTPUT -p udp -j $filtername &> /dev/null
+	iptables -X $filtername &> /dev/null
+
+	iptables-restore --noflush < $lib/filter
+	iptables -I INPUT 4 -p tcp --dport $portstart:$portstop -j $filtername
+	iptables -I INPUT 4 -p udp --dport $portstart:$portstop -j $filtername
+	iptables -I OUTPUT 1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p tcp -j $filtername
+	iptables -A OUTPUT -p udp -j $filtername
+	exit
+
 else
 	echo "Usage: $0 <command>"
 	echo "commands:"
-	echo "	update (updates the filter file from the online blocklists"
-	echo "	start  (adds ips to drop list, as well as adds $portstart-$portstop to open ports)"
-	echo "	stop   (opposite of start)"
+	echo "	$update (updates the filter file from the online blocklists"
+	echo "	$start  (adds ips to drop list, as well as adds $portstart-$portstop to open ports)"
+	echo "	$stop   (opposite of start)"
+	echo "	$reload (reloads the blocklist)"
 fi
